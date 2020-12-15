2045c2fda7bc86186861446939de68e9f86032cc
NATKAMONLAK <paintlpch@gmail.com>
2020-12-15 15:51:55 +0700

    lab5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+    int x[1000];
+    int i = 1,j = 0,k = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x[0];
+    if(x[0] == 0){
+        j=0;
+        k=0;
+    }
+    else do{
+        cout << "Enter an integer: ";
+        cin >> x[i];
+        i++;
+        if(x[i-1] %2 == 0){
+            j++;
+        }
+        else{
+            k++;
+        }
+    }while(x[i-1] != 0);
+    cout << "#Even numbers = " << j;
+    cout << "\n#Odd numbers = " << k;
     return 0;
 }
